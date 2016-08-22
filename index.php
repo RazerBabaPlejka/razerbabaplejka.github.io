4fdbc4227639a609dec3375efefc33c80d2cb3d0
RazerBabaPlejka <niko170803@gmail.com>
2016-08-22 16:20:21 +0200

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>RazerBabaPlejka</title>
+<style>
+h1 {
+	background-color: red;
+	color: blue;
+	}
+</style>
+</head>
+<body>
+<h1>Welcome to RazerBabaPlejka</h1>
+</body>
+</html>
