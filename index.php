bb9e1e6b413dc634cdda07ed53d1fb457bf1ada1
razerbabaplejka <niko170803@gmail.com>
2016-08-23 07:46:27 +0000

    f

--- index.php
+++ index.php
@@ -1,14 +1,37 @@
 <html>
 <head>
-<title>RazerBabaPlejka</title>
+<title>RBP Web</title>
 <style>
+body {
+     background-image:url('https://newevolutiondesigns.com/images/freebies/white-wallpaper-18.jpg');
+  }
+}
 h1 {
-	background-color: red;
-	color: blue;
-	}
+	color: red;
+}
+p {
+  color: blue;
+}
+.active {
+      color: lime;
+}
+.dominiko {
+	color: white;
+}
 </style>
 </head>
 <body>
-<h1>Welcome to RazerBabaPlejka</h1>
+  <h1><center>Welcome to <b>RazerBabaPlejka.github.io </b> </center></h1>
+  <p>Hello World! <b>This site is in building progress</b>
+  <p><a href="http://bit.ly/rbpYT">RBP YouTube</a>
+   <img src="http://gulin.sg/andrea/maple/CoverFlow/png/img1.png" alt="RBP Web" style="width:512px;height:512px;">
+<br>
+<div class="active">
+<h3>Active RBP Team:</h3>
+</div>
+<div class="dominiko">
+<p>N!k0</p>
+<p>_D0MINIK_</p>
+</div>
 </body>
 </html>
